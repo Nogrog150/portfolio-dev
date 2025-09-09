afe4a1e163cdd46375b51bf4ed2a09c06a4200a4
Nog <maurinho1402@hotmail.com>
2025-09-09 20:34:12 -0300

    Iniciando projeto do portfolio

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import Navbar from "./styles/Navbar";
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-900 bg-gradient-to-br from-purple-900 to-gray-900 text-white">
+      <Navbar></Navbar>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/styles/Navbar.jsx
@@ -0,0 +1,20 @@
+function navbar() {
+  return (
+    <div>
+      <header className="flex ">
+        <h3>DevMauro</h3>
+        <nav>
+          <ul>
+            <li>Inicio</li>
+            <li>Sobre</li>
+            <li>Habilidades</li>
+            <li>Projetos</li>
+            <li>Contato</li>
+          </ul>
+        </nav>
+      </header>      
+    </div>
+  )
+}
+
+export default navbar
