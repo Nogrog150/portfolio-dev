f699c89a110190d10015f8717dc09b89279bcc95
Nog <maurinho1402@hotmail.com>
2025-09-15 23:19:44 -0300

    Arrumando componentes

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import Navbar from "./styles/Navbar"
 import Sobre from "./components/Sobre"
 import Habilidades from "./components/Habilidades";
+import HeroSection from "./components/HeroSection";
+
 function App() {
   return (
     <div className="min-h-screen bg-[#111827] text-white">
       <Navbar></Navbar>
+      <HeroSection></HeroSection>
       <Sobre></Sobre>
       <Habilidades></Habilidades>
     </div>
--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,10 +1,28 @@
+import FotoPerfil from "../assets/foto-perfil.png";
 
 function HeroSection() {
   return (
-    <div>
-      
+    <div className="flex flex-row justify-evenly items-center max-lg:flex-col mb-60 max-lg:gap-10 pt-30">
+      <div className="flex flex-col gap-3">
+        <h1 className="font-bold text-5xl w-80">
+          <span className="text-purple-700">Olá</span>, eu sou o Mauro
+        </h1>
+        <p className="text-gray-400 text-xl
+        /* -- responsividade -- */
+
+
+        ">Sou um desenvolvedor frontend</p>
+      </div>
+
+      <div className="w-72 h-72 rounded-full overflow-hidden flex justify-center border-4 border-black items-center">
+        <img
+          src={FotoPerfil}
+          alt="Foto de perfil"
+          className="w-full h-full object-cover"
+        />
+      </div>
     </div>
-  )
+  );
 }
 
-export default HeroSection
+export default HeroSection;
